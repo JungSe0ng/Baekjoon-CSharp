9c54de5b023f639ec4268832d9a2256abb996593
SeongKwon <128352351+JungSe0ng@users.noreply.github.com>
2025-11-24 04:38:24 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5316 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Baekjoon_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+        }
+    }
+}
