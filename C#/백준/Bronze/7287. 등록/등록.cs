6dbef9b28dc8074f0c1134f45b6833b006a1a736
SeongKwon <128352351+JungSe0ng@users.noreply.github.com>
2025-11-24 04:36:53 +0900

    [Bronze V] Title: 등록, Time: 40 ms, Memory: 5368 KB -BaekjoonHub

---
+++ C#/백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Baekjoon_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int corretAnswersCnt = 124;
+            string myId = "calmdrone";
+            Console.WriteLine($"{corretAnswersCnt}");
+            Console.WriteLine($"{myId}");
+        }
+    }
+}
