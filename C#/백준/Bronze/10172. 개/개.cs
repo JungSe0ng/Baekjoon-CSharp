ed0f87e91f87e759820cb3ee2f596773d87f94bc
SeongKwon <128352351+JungSe0ng@users.noreply.github.com>
2025-11-24 04:39:33 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5308 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Baekjoon_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+        }
+    }
+}
