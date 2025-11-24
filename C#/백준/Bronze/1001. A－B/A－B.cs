ec57e74b4f8e4ac99edbb8b5dcd154ea660a65aa
SeongKwon <128352351+JungSe0ng@users.noreply.github.com>
2025-11-24 23:04:59 +0900

    [Bronze V] Title: A-B, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ C#/백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Baekjoon_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] inputArray = Console.ReadLine().Split(' ');
+            int A = int.Parse(inputArray[0]);
+            int B = int.Parse(inputArray[1]);
+
+            Console.WriteLine(A - B);
+        }
+    }
+}
